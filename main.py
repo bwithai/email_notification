fa0a9b723841faa14d059284a1a43993cb18ec16
sanaullah <sanaullah@git.com>
2022-02-18 15:07:02 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import ValidationError
+
+from database import session
+from models import EmailSchedule
+from schemas import SendingEmailSchema
+from send_email import send_email_background
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return 'Wellcome to Home'
+
+
+@app.get('/send-email/backgroundtasks')
+def send_email_backgroundtasks(background_tasks: BackgroundTasks):
+    emails: EmailSchedule = session.query(EmailSchedule).all()
+    for email in emails:
+        try:
+            send_email_background(background_tasks, email.title, email.email_to, email.message)
+        except ValidationError as error:
+            email.status = str(error)
+            session.commit()
+            return str(error)
+
+        email.status = "Email send Successfully"
+        session.commit()
+        return {
+            "message": "Email send Successfully"
+        }
+
+
+@app.post('/subscribe')
+def send_email_queue(data: SendingEmailSchema):
+    with session as db_session:
+        new_entry = EmailSchedule(title=data.title, email_to=data.email, message=data.message, status="")
+        print("here \n")
+        db_session.add(new_entry)
+        db_session.commit()
+        db_session.rollback()
+        return {
+            "message": "Added successfully"
+        }
